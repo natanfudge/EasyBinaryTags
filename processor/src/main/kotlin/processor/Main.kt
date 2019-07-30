453e80d728466762d2afc823517f7058058e1c3c
natanfudge <natan.lifsiz@gmail.com>
2019-07-30 12:31:49 +0300

    first commit

---
+++ processor/src/main/kotlin/processor/Main.kt
@@ -0,0 +1,9 @@
+@file:JvmName("Main")
+package processor
+
+import core.*
+
+fun main() {
+    val answer = DeepThought.compute()
+    println("The answer to the ultimate question of Life, the Universe and Everything is $answer.")
+}
