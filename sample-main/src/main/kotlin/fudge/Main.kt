4ac34704ac245ff23d1a7bb1fadbef7b1d7245a2
natanfudge <natan.lifsiz@gmail.com>
2019-07-30 15:11:09 +0300

    Migrations to theft

---
+++ sample-main/src/main/kotlin/fudge/Main.kt
@@ -0,0 +1,22 @@
+package fudge
+
+import fudge.Generated_Hello
+import fudge.toJson
+
+@GenName
+class Hello
+
+@Kson
+data class User(val name: String,
+                val email: String)
+
+fun main() {
+    println("Hello ${Generated_Hello().getName()}")
+    println("User to JSON")
+    val user = User(
+            name = "Test",
+            email = "test@email.com"
+    )
+    println("User: $user")
+    println("Json: ${user.toJson()}")
+}
